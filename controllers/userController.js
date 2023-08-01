5112887a295ce1b5c5a19c7142b63ad02d9bb4f3
unknown <vidhuprasadcp619@gmail.com>
2023-08-01 19:50:24 +0530

    userController.js updated

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const mongoose=require('mongoose');
+const USER=require('../models/userModel').users;
+
+const homePage=(req,res)=>{
+    res.render("home.hbs");
+}
+
+const signupPage=(req,res)=>{
+    res.render('signup.hbs');
+}
+
+const loginPage=(req,res)=>{
+    res.render('login.hbs');
+    
+}
+
+const homePage2=(req,res)=>{
+    res.render('home2.hbs');
+}
+
+const doSignUp=(req,res)=>{
+    console.log(req.body);
+    USER({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    }).save().then(()=>{
+        res.json({signup:true})
+    }).catch((err)=>{
+        console.error('Error saving user:', err);
+        res.json({signup: false});
+    })
+    
+
+}
+
+const doLogIn=(req,res)=>{
+    USER.find({email:req.body.email,password:req.body.password}).then((response)=>{
+        if(response.length > 0){
+            res.json({login:true});
+        }else{
+            res.json({login:false});
+        }
+    })
+}
+
+
+module.exports={signupPage,loginPage,homePage,doSignUp,doLogIn,homePage2}
